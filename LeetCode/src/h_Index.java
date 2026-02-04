007c209bccb6cb05ac371bb5117f727a0ad66c80
Anik Ghosh <anikghos4355@gmail.com>
2026-02-05 02:02:14 +0530

    Leet Code - 274, h-Index , using sort

---
+++ LeetCode/src/h_Index.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class h_Index {
+    public static int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int i = 0;
+        for(int j = citations.length-1; j>=0; j--){
+            if(citations[j] > i){
+                i++;
+            }else {
+                break;
+            }
+        }
+
+        return i;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,1};
+        System.out.println(hIndex(arr));
+    }
+
+}
