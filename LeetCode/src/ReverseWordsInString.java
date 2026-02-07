d336c99503a5129b9d0ce792d80a1e3464a53204
Anik Ghosh <anikghos4355@gmail.com>
2026-02-07 13:02:28 +0530

    LeeT code - 151, Reverse String by Words

---
+++ LeetCode/src/ReverseWordsInString.java
@@ -0,0 +1,19 @@
+public class ReverseWordsInString {
+    public static void main(String[] args) {
+        String s = "  hello world  ";
+        String ans = reverseWords(s);
+        System.out.println(ans);
+
+    }
+    public static String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+        StringBuilder sb = new StringBuilder();
+        for(int i = words.length-1;i>=0; i--){
+            sb.append(words[i]);
+            if(i!=0){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
