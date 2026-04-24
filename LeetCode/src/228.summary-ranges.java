07ccb5066898a0acd371a9c1cf63d82be16fa58a
Anik Ghosh <anikghosh4355@gmail.com>
2026-04-24 18:39:17 +0530

    Ranges with the fail required logic cases

---
+++ LeetCode/src/228.summary-ranges.java
@@ -0,0 +1,100 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=228 lang=java
+ *
+ * [228] Summary Ranges
+ *
+ * https://leetcode.com/problems/summary-ranges/description/
+ *
+ * algorithms
+ * Easy (54.13%)
+ * Likes:    4543
+ * Dislikes: 2386
+ * Total Accepted:    940.2K
+ * Total Submissions: 1.7M
+ * Testcase Example:  '[0,1,2,4,5,7]'
+ *
+ * You are given a sorted unique integer array nums.
+ * 
+ * A range [a,b] is the set of all integers from a to b (inclusive).
+ * 
+ * Return the smallest sorted list of ranges that cover all the numbers in the
+ * array exactly. That is, each element of nums is covered by exactly one of
+ * the ranges, and there is no integer x such that x is in one of the ranges
+ * but not in nums.
+ * 
+ * Each range [a,b] in the list should be output as:
+ * 
+ * 
+ * "a->b" if a != b
+ * "a" if a == b
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [0,1,2,4,5,7]
+ * Output: ["0->2","4->5","7"]
+ * Explanation: The ranges are:
+ * [0,2] --> "0->2"
+ * [4,5] --> "4->5"
+ * [7,7] --> "7"
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [0,2,3,4,6,8,9]
+ * Output: ["0","2->4","6","8->9"]
+ * Explanation: The ranges are:
+ * [0,0] --> "0"
+ * [2,4] --> "2->4"
+ * [6,6] --> "6"
+ * [8,9] --> "8->9"
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 0 <= nums.length <= 20
+ * -2^31 <= nums[i] <= 2^31 - 1
+ * All the values of nums are unique.
+ * nums is sorted in ascending order.
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        
+        if (nums.length == 0) return result;
+
+        int start = nums[0];
+
+        for (int i = 1; i <= nums.length; i++) {
+            if (i == nums.length || nums[i] != nums[i - 1] + 1) {
+                
+                if (start == nums[i - 1]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i - 1]);
+                }
+
+                if (i < nums.length) {
+                    start = nums[i];
+                }
+            }
+        }
+
+        return result;
+    }
+}
+// @lc code=end
+
