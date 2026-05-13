eec8f886fec4fdff5947ed7ad989ba3dd594a476
Anik Ghosh <anikghosh4355@gmail.com>
2026-05-13 22:29:13 +0530

    Test_1 classrom question no - 1

---
+++ College/src/danceGroup.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class danceGroup {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the String : ");
+        String str = sc.next();
+        System.out.println(numberOfPair(str));
+    }   
+    public static int numberOfPair(String str){
+        if(str.length() == 1 || str.length() == 0) return 0;
+        int i = 1;
+        int count = 0;
+        while(i< str.length()){
+            if(str.charAt(i) != str.charAt(i-1)){
+                count ++;
+                i = i+2;
+            }else{
+                i++;
+            }
+        }
+        return count;
+    }
+}
